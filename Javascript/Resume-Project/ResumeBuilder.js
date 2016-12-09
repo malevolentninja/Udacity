629746940948c12c2726334f606200cfad166d60
Olivia Boertje <malevolentninja@gmail.com>
2016-12-09 02:18:39 +0000

    Update ResumeBuilder.js

--- Javascript/Resume-Project/ResumeBuilder.js
+++ Javascript/Resume-Project/ResumeBuilder.js
@@ -1,82 +1,3 @@
-
-
-//var firstname =  "Olivia";
-
-//var age = "22";
-
-//console.log(firstname);
-
-
-
-//var email = "hjhoa@gmail.com";
-
-// var newEmail =
-    //email.replace("olivia", "fdgfhjki");
-
-//console.log(email);
-//conosle.log(newEmail);
-
-
-/* var name = "OlIViA BOertJe";
-
-function nameChanger(oldName) {
-    var finalName = oldName;
-    var names = oldName.split(" ");
-    names[1] = names[1].toUpperCase();
-    names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
-    finalName = names.join(" ");
-    return finalName;
-};
-
-console.log(nameChanger(name));
-
-*/
-
-//var awesomeThoughts = "My name is Olivia and I am AWESOME!";
-
-//console.log(awesomeThoughts);
-
-
-//var funThoughts =
-
-    //awesomeThoughts.replace("AWESOME", "FUN");
-
-
-
-// $("#main").append(funThoughts);
-
-
-
-
-var name = "Olivia";
-var formattedName = HTMLheaderName.replace("%data%", name);
-
-
-var role = "Web Developer";
-var formattedRole = HTMLheaderRole.replace("%data%", role );
-
-
-$("#header").prepend(formattedName);
-$("#header").prepend(formattedRole);
-
-
-/* var skills = 
-    ["awesomeness","programming", "Html", "CSS", "Js" ];
-
-$("#main").append(skills);
-
-$("#main").append(skills[0]);
-
-$("#main").append(skills.length);
-*/
-
-/* var img = document.createElement("img");
-    img.src="images/me.png";
-    var src = document.getElementById("header");
-src.appendChild(img);
-
-*/
-
 var bio = {};
     "name" : "Olivia",
      "age" : 22,
@@ -107,18 +28,6 @@ $("#main").append(bio.city);
 // bio ["city"] = "London";
 //$("#main").append(bio["city"]);
 
-
-
-
-
-
-
-
-
-
-
-
-
 var work = {};
     work.position = "Customer Assistant";
     work.employer = "Marks and Spencers";
@@ -127,15 +36,40 @@ var work = {};
 
 
                        
-var education = {}; 
-  education["name"] = "MHCHS";
-   education["years"] = 2006-2013;
-   education ["city"] = "London" ; 
+var education = {
+    "schools": [
 
-$("#main").append(work["position"]);               
-$("#main").append(education.name);
+	{
+
+		"name": "Mill Hill",
+		"years": 2011 - 2013,
+		"city": "London",
+		"certifications": " A Levels",
+		"details": ["A* Geography , "
+			"A Politics , "
+			"B History"
+
+		],
+
+		"name": "Exeter University",
+		"years": 2013 - 2016,
+		"city": "Exeter",
+		"degree": "BA Geography",
+
+	}
+],
+"onlineCourses": [{
+	"title": "Javascript Syntax",
+	"Schools": "Udacity",
+	"Dates": 2016,
+	"url": "https://www.udacity.com/course/javascript-basics--ud804"
+}]
+};
+//$("#main").append(work["position"]);               
+//$("#main").append(education.name);
 
 
+// JSON nested data use 
 
 
 
