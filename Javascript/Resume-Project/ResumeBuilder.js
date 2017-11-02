75ed563f99d98aacaa7ad45345ce868a483169cf
Olivia Boertje <malevolentninja@users.noreply.github.com>
2017-11-02 20:32:48 +0000

    Update ResumeBuilder.js
    work jobs section edited and improved formatting

--- Javascript/Resume-Project/ResumeBuilder.js
+++ Javascript/Resume-Project/ResumeBuilder.js
@@ -2,15 +2,15 @@ var bio {
     "name": "Olivia Boertje",
     "role": "Aspiring Developer", 
     "contacts": {
-       "mobile": "1234 1234 123",
-       "email" : "malevolentninja@gmail.c",
+       "email" : "malevolentninja@gmail.com",
        "github": "malevolentninja",
        "twitter": "@Olivia_Boertje",
        "location": "London"
                   },
     "welcomeMessage" : "Thankyou for visting my page, enjoy looking around!",
     "skills": [
-    "HTML5", "CSS/SCSS", "Gulp", "JavaScript" , "jQuery", "Git", "NodeJS" ,"Ruby", "Kitesurfing", "Shaolin Kung Fu", "Tai Chi", "Wing Chun", "Yoga" "Playing the Oboe and Clarinet"],
+    "HTML5", "CSS/SCSS", "Gulp", "JavaScript" , "jQuery", "Git", "NodeJS" ,"Ruby"],
+    "interests": [ "Kitesurfing", "Shaolin Kung Fu", "Tai Chi", "Wing Chun", "Yoga" "Playing the Oboe and Clarinet"],
     "bioPic": "images/me.jpg"
 };
 
@@ -91,36 +91,36 @@ var education = {
 
 var work = {
 
-    "Jobs"[ 
+    "Jobs": [ 
 
-	{
-	work.position = "Education Catalyst Project Coordinator";
-	work.employer = "Exeter University";
-	work.years = 0.6;
-	work.dates = "7th 'January -  7th July 2017";
-	work.description  = [{ "A critical review of course modules in accordance with the university's education strategy. " 
-	"▪ Trained to conduct focus groups, edit ELE pages and project management course"  
-	"▪ Collaborating with a range of stakeholders " 
-        "▪ Writing agendas, minutes and chairing meetings" 
-	"▪ Working with and managing a team"
-	"▪ Utilising Microsoft Office tools"
-	"▪ Organised events and organised the production of our webpage on the University website."	
-}]; 
-	
-},
-	
-    {
-    work.position = "Customer Assistant";
-    work.employer = "Marks and Spencers";
-    work.years = 0.3;
-work.dates = "4th October 2016 - 5th January 2017"
-    work.description = 
-        "This role involves helping customers find products, ensure the shops standards are well amaintained(e.g. neat, full shelves) "
-    "Needed to have good knowledge of beers, wines, spirits and other drinks to help the customer make an informed decision about the products they wish to purchase."
-
-},
-    
-    ]
+          {
+                work.position = "Education Catalyst Project Coordinator";
+                work.employer = "Exeter University";
+                work.years = 0.6;
+                work.dates = "7th 'January -  7th July 2017";
+                work.description  = [ "A critical review of course modules in accordance with the university's education strategy. ", "▪ Trained to conduct focus groups, edit ELE pages and project management course", "▪ Collaborating with a range of stakeholders ", "▪ Writing agendas, minutes and chairing meetings", "▪ Working with and managing a team", "▪ Utilising Microsoft Office tools", "▪ Organised events and helped produce a webpage of our results on the University website"], 
+           },
+           { 
+                work.position = "Beers, Wines and Spirits Hero";
+                work.employer = "Marks and Spencers";
+                work.years = 0.3;
+                work.dates = "4th October 2016 - 5th January 2017"
+                work.description = ["This role involves helping customers find products, ensure the shops standards are well amaintained(e.g. neat, full shelves)", "I studied different types of beers, wines, spirits and the processes involved inorder to help the customer choose a product suitable for their criteria."],
+           },
+           { 
+                work.position = "Back Stage Manager";
+                work.employer = "Mill Hill County School";
+                work.years = 3.0;
+                work.dates = "December 2009 - December 2012"
+                work.description = [" Communicating with performers and staff" ,"Time management and organisational skills to ensure smooth running of concerts"],  
+           },
+                work.position = "Intern";
+                work.employer = "Bond Financial Insurance";
+                work.years = 0.1;
+                work.dates = "July 2009 - August 2009"
+                work.description = [" Strong computer skills to update information and conduct administrative tasks", "Answer phone calls and speak to clients", "Introduction of a recycling scheme "],  
+           },
+    ];
     
 /*
  {
@@ -133,7 +133,7 @@ work.dates = "4th October 2016 - 5th January 2017"
  }
 */
 
-},
+
 
 	
 var projects = {
